318ff58ead78d1f5975a43507ffee2eb9b44f85a
Catherinelimm <124689577+Catherinelimm@users.noreply.github.com>
2024-04-15 19:49:21 +0100

    debug

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -11,58 +11,73 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const sendShopToken = require("../utils/shopToken");
 
 // create shop
-router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const sellerEmail = await Shop.findOne({ email });
-    if (sellerEmail) {
-      return next(new ErrorHandler("User already exists", 400));
-    }
-
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-      folder: "avatars",
-    });
-
-
-    const seller = {
-      name: req.body.name,
-      email: email,
-      password: req.body.password,
-      avatar: {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      },
-      address: req.body.address,
-      phoneNumber: req.body.phoneNumber,
-      zipCode: req.body.zipCode,
-    };
-
-    const activationToken = createActivationToken(seller);
-
-    const activationUrl = `https://gadget-redux-tutorial.vercel.app/${activationToken}`;
-
+router.post(
+  "/create-shop",
+  catchAsyncErrors(async (req, res, next) => {
     try {
-      await sendMail({
-        email: seller.email,
-        subject: "Activate your Shop",
-        message: `Hello ${seller.name}, please click on the link to activate your shop: ${activationUrl}`,
-      });
-      res.status(201).json({
-        success: true,
-        message: `please check your email:- ${seller.email} to activate your shop!`,
-      });
+      const { email, name, password, avatar, address, phoneNumber, zipCode } =
+        req.body;
+      const sellerEmail = await Shop.findOne({ email });
+
+      console.log("REQ BODY", req.body);
+
+      const defaultshopavatar = {
+        public_id: "avatars/defaultshopavatar",
+        url: "https://res.cloudinary.com/dzr4sqtwh/image/upload/v1712869912/avatars/defaultshopavatar.png",
+      };
+
+      if (sellerEmail) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
+
+      let seller = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: defaultshopavatar,
+        address: address,
+        phoneNumber: phoneNumber,
+        zipCode: zipCode,
+      };
+
+      if (avatar) {
+        const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+          folder: "avatars",
+        });
+
+        seller.avatar = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      const activationToken = createActivationToken(seller);
+
+      const activationUrl = `https://gadget-redux.vercel.app/seller/activation/${activationToken}`;
+
+      try {
+        await sendMail({
+          email: seller.email,
+          subject: "Activate your Shop",
+          message: `Hello ${seller.name}, please click on the link to activate your shop: ${activationUrl}`,
+        });
+        res.status(201).json({
+          success: true,
+          message: `Please check your email:- ${seller.email} to activate your shop!`,
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
     } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
+      return next(new ErrorHandler(error.message, 400));
     }
-  } catch (error) {
-    return next(new ErrorHandler(error.message, 400));
-  }
-}));
+  })
+);
 
 // create activation token
 const createActivationToken = (seller) => {
   return jwt.sign(seller, process.env.ACTIVATION_SECRET, {
-    expiresIn: "5m",
+    expiresIn: "30m",
   });
 };
 
@@ -73,35 +88,43 @@ router.post(
     try {
       const { activation_token } = req.body;
 
+      console.log("Activation Token :", activation_token);
+
+      // Verify the activation token
       const newSeller = jwt.verify(
         activation_token,
         process.env.ACTIVATION_SECRET
       );
 
+      // Check if the token is valid
       if (!newSeller) {
-        return next(new ErrorHandler("Invalid token", 400));
+        return next(new ErrorHandler("Invalid activation token", 400));
       }
+
       const { name, email, password, avatar, zipCode, address, phoneNumber } =
         newSeller;
 
-      let seller = await Shop.findOne({ email });
-
-      if (seller) {
+      // Check if the user already exists
+      let existingSeller = await Shop.findOne({ email });
+      if (existingSeller) {
         return next(new ErrorHandler("User already exists", 400));
       }
 
-      seller = await Shop.create({
+      // Create a new user
+      const seller = await Shop.create({
         name,
         email,
-        avatar,
         password,
+        avatar,
         zipCode,
         address,
         phoneNumber,
       });
 
+      // Send response with token
       sendShopToken(seller, 201, res);
     } catch (error) {
+      // Handle errors
       return next(new ErrorHandler(error.message, 500));
     }
   })
@@ -172,7 +195,7 @@ router.get(
         sameSite: "none",
         secure: true,
       });
-      
+
       res.status(201).json({
         success: true,
         message: "Log out successful!",
@@ -207,26 +230,25 @@ router.put(
     try {
       let existsSeller = await Shop.findById(req.seller._id);
 
-        const imageId = existsSeller.avatar.public_id;
+      const imageId = existsSeller.avatar.public_id;
 
-        await cloudinary.v2.uploader.destroy(imageId);
+      await cloudinary.v2.uploader.destroy(imageId);
 
-        const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-          folder: "avatars",
-          width: 150,
-        });
+      const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 150,
+      });
 
-        existsSeller.avatar = {
-          public_id: myCloud.public_id,
-          url: myCloud.secure_url,
-        };
+      existsSeller.avatar = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
 
-  
       await existsSeller.save();
 
       res.status(200).json({
         success: true,
-        seller:existsSeller,
+        seller: existsSeller,
       });
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
